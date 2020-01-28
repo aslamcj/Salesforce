8d09e7ccfda6b0cc21d685d5fc255d7a32bf2588
Aslam <aslamcj@gmail.com>
2020-01-28 15:36:45 +0530

    Update CustomProduct.js

--- exercise 2/app_custom_demo/cartridge/controllers/CustomProduct.js
+++ exercise 2/app_custom_demo/cartridge/controllers/CustomProduct.js
@@ -2,17 +2,20 @@
 
 var server = require('server');
 var cache = require('*/cartridge/scripts/middleware/cache');
-var ProductMgr = require('dw/catalog/ProductMgr');
+var ProductMgr = require('dw/catalog/ProductMgr'); // Including exiting file ProductMgr
 
 server.get('Show', cache.applyDefaultCache, function (req, res, next) {
 	
-	var pid = req.querystring.PID;
-	var apiProduct = ProductMgr.getProduct(pid);
-    res.render('helloworld', {
-    	Message: 'Hello World! Again.',
-    		pid : apiProduct.ID
-    });
-    next();
+	var pid = req.querystring.PID; // reading query paramenter value requested in URL
+	var apiProduct = ProductMgr.getProduct(pid); // Passing back query param value to exting ProductMgr API function
+        
+	// apiProduct.ID fetching product id from API and passing back to 'helloworld.isml'
+	res.render('helloworld', {
+    	  Message: 'Hello World! Again.',
+    	  pid : apiProduct.ID
+    	});
+    	
+	next();
 });
 
 module.exports = server.exports();
